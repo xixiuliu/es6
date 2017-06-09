b8238a4b8ad30898a3312b6647147193d3d3a3de
unknown <liuxixiu@kingyeedev.cn>
2017-06-09 14:22:34 +0800

    es6项目所有的示例及实战源码

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,5 @@
-// import './class/lesson1';
-// import 'babel-polyfill';
-// import './class/lesson17';
-// import{A,test,Hello} from './class/lesson17';
-// import{A} from './class/lesson17';
-// import * as lesson from './class/lesson17';
+import 'babel-polyfill';//es6兼容
+import Lottery from './lottery';
 
-import lesson17 from './class/lesson17';
-
-// console.log(A,test,Hello);
-// console.log(A);
-// console.log(lesson.A,lesson.test);
-console.log(lesson17.A);
+const syy=new Lottery();
+console.log(syy);
---
+++ app/js/lottery/interface.js
@@ -0,0 +1,67 @@
+import $ from 'jquery';
+
+class Interface{
+	//获取遗漏数据-issue是期号
+	getOmit(issue){
+		let self=this;
+		return new 	Promise((resolve,reject)=>{
+			$.ajax({
+				url:'/get/omit',
+				data:{
+					issue:issue
+				},
+				dataType:'json',
+				success:function(res){
+					self.setOmit(res.data);
+					resolve.call(self,res);
+				},
+				error:function(err){
+					reject.call(err);
+				}
+			})
+		});
+	}
+
+	//获取开奖号码
+	getOpenCode(issue){
+		let self=this;
+		return new Promise((resolve,reject)=>{
+			$.ajax({
+				url:'/get/opencode',
+				data:{
+					issue:issue
+				},
+				dataType:'json',
+				success:function(res){
+					self.setOpenCode(res.data);
+					resolve.call(self,res);
+				},
+				error:function(err){
+					reject.call(err);
+				}
+			})
+		});
+	}
+
+	//获取当前状态
+	getState(issue){
+		let self=this;
+		return new Promise((resolve,reject)=>{
+			$.ajax({
+				url:'/get/state',
+				data:{
+					issue:issue
+				},
+				dataType:'json',
+				success:function(res){
+					resolve.call(self,res);
+				},
+				error:function(err){
+					reject.call(err);
+				}
+			})
+		});
+	}
+}
+
+export default Interface
